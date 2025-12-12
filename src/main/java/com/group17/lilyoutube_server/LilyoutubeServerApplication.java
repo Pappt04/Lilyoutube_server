03e6c9846f7ff4f9f3a2a2df835b9761c67d9a9e
Pappt04 <69700151+Pappt04@users.noreply.github.com>
2025-12-12 15:40:38 +0100

    Initial commit

---
+++ src/main/java/com/group17/lilyoutube_server/LilyoutubeServerApplication.java
@@ -0,0 +1,13 @@
+package com.group17.lilyoutube_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilyoutubeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LilyoutubeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group17/lilyoutube_server/LilyoutubeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group17.lilyoutube_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LilyoutubeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
